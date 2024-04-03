dc2ee983f03db706c285951f25bb0abd34bb967f
Mizuyamu <146126925+Mizuyamu@users.noreply.github.com>
2024-04-03 12:02:55 -0700

    push changes

--- 3D FPS Project/Assets/Models/Scripts/GameManager.cs
+++ 3D FPS Project/Assets/Models/Scripts/GameManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI targetText; 
+    private int _targetAmount;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        int floatingTargetAmount = GameObject.FindGameObjectsWithTag("Floating Target").Length;
+        int standingTargetAmount = GameObject.FindGameObjectsWithTag("Targets").Length;
+        _targetAmount = floatingTargetAmount + standingTargetAmount;
+        targetText.text = "Targets: " + _targetAmount.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateTargetAmount(int amount)
     {
-        
+        _targetAmount += amount;
     }
 }
--- 3D FPS Project/Assets/Models/Scripts/Target.cs
+++ 3D FPS Project/Assets/Models/Scripts/Target.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Target : MonoBehaviour
 {
+     void Start()
+        {
+            this.gameObject.SetActive(true);
+        }
    void OnCollisionEnter(Collision other)
    {
         if(other.gameObject.CompareTag("Bullet"))
@@ -11,7 +16,9 @@ public class Target : MonoBehaviour
             //add points to the score
             if(this.gameObject.tag == "Floating Target")
             {
-                Destroy(this.gameObject);
+                //Destroy(this.gameObject);
+                GameObject.Find("Game Manager").GetComponent<GameManager>.()UpdatetargetAmount(-1)
+                this.gameObject.SetActive(false);
             }
             //add grayscale to standing target later on
         }
